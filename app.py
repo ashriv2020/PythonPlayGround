3f7331be2fea46ea210460cd7abc3c9c27736c0f
DotNetAbhishek <50742393+ashriv2020@users.noreply.github.com>
2024-06-02 22:35:52 +0530

    feat: Add code for creating sets, lists, dictionaries, tuples, strings, and ranges of numbers
    This commit adds code to create different types of collections for numbers, including sets, lists, dictionaries, tuples, strings, and ranges. Each collection is printed to the console for demonstration purposes.

--- app.py
+++ app.py
@@ -1,2 +1,31 @@
 # print hello world
 print("Hello, World!")
+
+#create a set of numbers
+numbers = {1, 2, 3, 4, 5}
+print('a set of numbers',numbers)
+
+# create a list of numbers
+numbers = [1, 2, 3, 4, 5]
+print('a list of numbers',numbers)
+
+# create a dictionary of numbers
+numbers = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5}
+print('dictionary',numbers)
+
+# create a tuple of numbers
+numbers = (1, 2, 3, 4, 5)
+print('tupple',numbers)
+
+# create a string of numbers
+numbers = "12345"
+print('string',numbers)
+
+# create a range of numbers
+numbers = range(1, 6)
+print('range 1-6',numbers)
+
+
+
+
+
